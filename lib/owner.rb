c3f24c170d1e2a2e3fad18db3b6adc55c466a4e2
ebuchheim <emma.buchheim@gmail.com>
2019-01-23 19:47:51 -0800

    add pet logic

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,83 @@
 class Owner
   # code goes here
+  attr_reader :species
+  attr_accessor :name, :pets
+
+  @@owners = []
+
+  def initialize(species)
+    @species = species
+    @@owners << self
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_fish(fish)
+    new_fish = Fish.new(fish)
+    @pets[:fishes] << new_fish
+  end
+
+  def buy_dog(dog)
+    new_dog = Dog.new(dog)
+    @pets[:dogs] << new_dog
+  end
+
+  def buy_cat(cat)
+    new_cat = Cat.new(cat)
+    @pets[:cats] << new_cat
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets[:fishes].each do |fish|
+      fish.mood = "nervous"
+    end
+    @pets[:cats].each do |cat|
+      cat.mood = "nervous"
+    end
+    @pets[:dogs].each do |dog|
+      dog.mood = "nervous"
+    end
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def list_pets
+    dogs = @pets[:dogs].count
+    cats = @pets[:cats].count
+    fish = @pets[:fishes].count
+    "I have #{fish} fish, #{dogs} dog(s), and #{cats} cat(s)."
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.count
+    @@owners.count
+  end
+
+  def self.reset_all
+    @@owners = []
+  end
+
 end
